c207924dfd988c7700c7a2a1db9182f3f23f4fba
furqan <furqanullah717@gmail.com>
2025-03-30 19:11:19 +0500

    part 2

---
+++ app/src/main/java/com/codewithfk/musify_android/MainViewModel.kt
@@ -0,0 +1,27 @@
+package com.codewithfk.musify_android
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.codewithfk.musify_android.data.repository.StatusRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import org.koin.android.annotation.KoinViewModel
+
+@KoinViewModel
+class MainViewModel (val repository: StatusRepository) : ViewModel() {
+
+    private val state = MutableStateFlow("")
+    val status = state.asStateFlow()
+
+    init {
+        getStatus()
+    }
+
+    private fun getStatus() {
+        viewModelScope.launch {
+            val result = repository.getStatus()
+            state.value = result
+        }
+    }
+}
---
+++ app/src/main/java/com/codewithfk/musify_android/di/NetworkModule.kt
@@ -0,0 +1,45 @@
+package com.codewithfk.musify_android.di
+
+import com.codewithfk.musify_android.data.network.ApiService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.core.annotation.Module
+import org.koin.core.annotation.Single
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+class NetworkModule {
+
+    @Single
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+
+    @Single
+    fun provideOkHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    @Single
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        val gson = com.google.gson.GsonBuilder()
+            .setLenient()
+            .create()
+
+        return Retrofit.Builder()
+            .baseUrl("http://10.0.2.2:8080/") // Replace with your base URL
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+    }
+
+    @Single
+    fun provideApiService(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+}
